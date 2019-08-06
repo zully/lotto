acdc0f5054a587173531a24616b62edcbf769ef4
Chris H <zully_1@yahoo.com>
2019-08-06 13:15:55 -0500

    oop part #3

--- app.py
+++ app.py
@@ -16,32 +16,37 @@ app = Bottle(__name__)
 
 class Lotto:
 
-    def __init__(self):
+    def __init__(self, game_type):
+        self.game_type = game_type
         self.games = {'two_step': 'Texas Two Step',
                       'lotto_texas': 'Lotto Texas',
                       'mega_millions': 'Mega Millions',
                       'power_ball': 'Power Ball'}
-        self.two_step = {'pool': 35,
-                         'amount': 4,
-                         'exn_pool': 35,
-                         'game_code': 'WSD1S'}
-        self.lotto_texas = {'pool': 54,
-                            'amount': 6,
-                            'exn_pool': False,
-                            'game_code': 'WLD1JCMNS'}
-        self.mega_millions = {'pool': 70,
-                              'amount': 5,
-                              'exn_pool': 25,
-                              'game_code': 'WMD1JCMNS'}
-        self.power_ball = {'pool': 69,
-                           'amount': 5,
-                           'exn_pool': 26,
-                           'game_code': 'WPD1JCMNS'}
+
+        if game_type == 'two_step':
+            self.game_config = {'pool': 35,
+                                'amount': 4,
+                                'exn_pool': 35,
+                                'game_code': 'WSD1S'}
+        elif game_type == 'lotto_texas':
+            self.game_config = {'pool': 54,
+                                'amount': 6,
+                                'exn_pool': False,
+                                'game_code': 'WLD1JCMNS'}
+        elif game_type == 'mega_millions':
+            self.game_config = {'pool': 70,
+                                'amount': 5,
+                                'exn_pool': 25,
+                                'game_code': 'WMD1JCMNS'}
+        elif game_type == 'power_ball':
+            self.game_config = {'pool': 69,
+                                'amount': 5,
+                                'exn_pool': 26,
+                                'game_code': 'WPD1JCMNS'}
+        else:
+            self.game_config = None
 
     def get_num(pool):
-        if not pool:
-            return False
-
         num = str(randint(1, pool))
 
         if len(num) == 1:
@@ -49,45 +54,44 @@ class Lotto:
 
         return num
 
-    def get_exn(self, game_type):
+    def get_exn(self):
+        if not self.game_config['exn_pool']:
+            return False
 
-        return self.get_num(self.game_type['exn_pool'])
+        return self.get_num(self.game_config['exn_pool'])
 
-    def get_base_nums(self, game_type):
+    def get_base_nums(self):
         base_nums = []
 
-        while len(base_nums) < self.game_type['amount']:
-            temp = self.get_num(self.game_type['pool'])
+        while len(base_nums) < self.game_config['amount']:
+            temp = self.get_num(self.game_config['pool'])
 
             if temp not in base_nums:
                 base_nums.append(temp)
 
         return sorted(base_nums)
 
-    def get_game_code(self, game_type):
+    def get_game_code(self):
 
-        return self.game_type['game_code']
-
-
-lotto = Lotto()
+        return self.game_config['game_code']
 
 
 @app.route('/')
 def index():
-
+    lotto = Lotto(None)
     return template('index', version=VER, selected='',
                     games=lotto.games, base=False, exn=False)
 
 
 @app.route('/', method=['POST'])
-def give_nums():
+def results():
     game_type = str(request.forms.get('game_type'))
+    lotto = Lotto(game_type)
 
-    base = lotto.get_base_nums(game_type)
-    exn = lotto.get_num(game_type)
-    game_code = lotto.get_game_code(game_type)
+    base = lotto.get_base_nums()
+    exn = lotto.get_exn()
 
-    qrnums = 'LOT21:{}{}'.format(game_code, ''.join(base))
+    qrnums = 'LOT21:{}{}'.format(lotto.get_game_code(), ''.join(base))
 
     if exn:
         qrnums += exn
