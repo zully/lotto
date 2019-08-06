53be8d95d50bdbea75a6d9c41f9fbf0e6f5f84de
Chris H <zully_1@yahoo.com>
2019-08-06 13:29:35 -0500

    oop approach #4

--- app.py
+++ app.py
@@ -1,79 +1,15 @@
 #!bottle/bin/python
 
-#  Lotto Generator
-
 from bottle import (Bottle, request, template, TEMPLATE_PATH)
-from random import randint
 from pyqrcode import create
 from os import path
+from lotto import Lotto
 
 VER = '2.2.9'
 dir_path = path.dirname(path.realpath(__file__))
 TEMPLATE_PATH.insert(0, path.join(dir_path, 'views'))
 
 app = Bottle(__name__)
-
-
-class Lotto:
-
-    def __init__(self, game_type):
-        self.game_type = game_type
-        self.games = {'two_step': 'Texas Two Step',
-                      'lotto_texas': 'Lotto Texas',
-                      'mega_millions': 'Mega Millions',
-                      'power_ball': 'Power Ball'}
-
-        if game_type == 'two_step':
-            self.game_config = {'pool': 35,
-                                'amount': 4,
-                                'exn_pool': 35,
-                                'game_code': 'WSD1S'}
-        elif game_type == 'lotto_texas':
-            self.game_config = {'pool': 54,
-                                'amount': 6,
-                                'exn_pool': False,
-                                'game_code': 'WLD1JCMNS'}
-        elif game_type == 'mega_millions':
-            self.game_config = {'pool': 70,
-                                'amount': 5,
-                                'exn_pool': 25,
-                                'game_code': 'WMD1JCMNS'}
-        elif game_type == 'power_ball':
-            self.game_config = {'pool': 69,
-                                'amount': 5,
-                                'exn_pool': 26,
-                                'game_code': 'WPD1JCMNS'}
-        else:
-            self.game_config = None
-
-    def get_num(self, pool):
-        num = str(randint(1, pool))
-
-        if len(num) == 1:
-            num = '0{}'.format(num)
-
-        return num
-
-    def get_exn(self):
-        if not self.game_config['exn_pool']:
-            return False
-
-        return self.get_num(self.game_config['exn_pool'])
-
-    def get_base_nums(self):
-        base_nums = []
-
-        while len(base_nums) < self.game_config['amount']:
-            temp = self.get_num(self.game_config['pool'])
-
-            if temp not in base_nums:
-                base_nums.append(temp)
-
-        return sorted(base_nums)
-
-    def get_game_code(self):
-
-        return self.game_config['game_code']
 
 
 @app.route('/')
@@ -90,7 +26,6 @@ def results():
 
     base = lotto.get_base_nums()
     exn = lotto.get_exn()
-
     qrnums = 'LOT21:{}{}'.format(lotto.get_game_code(), ''.join(base))
 
     if exn:
---
+++ lotto.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python
+
+from random import randint
+
+
+class Lotto:
+
+    def __init__(self, game_type):
+        self.game_type = game_type
+        self.games = {'two_step': 'Texas Two Step',
+                      'lotto_texas': 'Lotto Texas',
+                      'mega_millions': 'Mega Millions',
+                      'power_ball': 'Power Ball'}
+
+        if game_type == 'two_step':
+            self.game_config = {'pool': 35,
+                                'amount': 4,
+                                'exn_pool': 35,
+                                'game_code': 'WSD1S'}
+        elif game_type == 'lotto_texas':
+            self.game_config = {'pool': 54,
+                                'amount': 6,
+                                'exn_pool': False,
+                                'game_code': 'WLD1JCMNS'}
+        elif game_type == 'mega_millions':
+            self.game_config = {'pool': 70,
+                                'amount': 5,
+                                'exn_pool': 25,
+                                'game_code': 'WMD1JCMNS'}
+        elif game_type == 'power_ball':
+            self.game_config = {'pool': 69,
+                                'amount': 5,
+                                'exn_pool': 26,
+                                'game_code': 'WPD1JCMNS'}
+        else:
+            self.game_config = None
+
+    def get_num(self, pool):
+        num = str(randint(1, pool))
+
+        if len(num) == 1:
+            num = '0{}'.format(num)
+
+        return num
+
+    def get_exn(self):
+        if not self.game_config['exn_pool']:
+            return False
+
+        return self.get_num(self.game_config['exn_pool'])
+
+    def get_base_nums(self):
+        base_nums = []
+
+        while len(base_nums) < self.game_config['amount']:
+            temp = self.get_num(self.game_config['pool'])
+
+            if temp not in base_nums:
+                base_nums.append(temp)
+
+        return sorted(base_nums)
+
+    def get_game_code(self):
+
+        return self.game_config['game_code']
