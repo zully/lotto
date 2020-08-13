3ea7d53648447f308555acb03f3cfbb6ecb7236d
Chris H <zully_1@yahoo.com>
2020-08-13 16:41:38 -0500

    change version

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from pyqrcode import create
 from os import path
 from lotto import Lotto
 
-VER = '2.3.1'
+VER = '2.3.2'
 dir_path = path.dirname(path.realpath(__file__))
 TEMPLATE_PATH.insert(0, path.join(dir_path, 'views'))
 
